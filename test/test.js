1c309a1e2100aeded8ea5c4541ea79332ec455cd
yanjie <jie.yan@ingeek.com>
2022-06-24 11:06:32 +0800

    style: 移除空格

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const read = function (path) {
   return fs.readFileSync(path, 'utf-8')
 }
 
-test('测试 font-weight转换', () => {
+test('测试font-weight转换', () => {
   const input = read('test/fixtures/normal.css')
   const output = read('test/fixtures/normal.out.css')
   expect(postcss(plugin({})).process(input).css).toEqual(output)
