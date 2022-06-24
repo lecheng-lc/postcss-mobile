8a21f7caf5c10cebe490905430ac669f6d8ff71f
yanjie <jie.yan@ingeek.com>
2022-06-24 11:05:46 +0800

    feat: 编写测试用例

--- test/test.js
+++ test/test.js
@@ -12,3 +12,25 @@ test('测试 font-weight转换', () => {
   const output = read('test/fixtures/normal.out.css')
   expect(postcss(plugin({})).process(input).css).toEqual(output)
 })
+test('测试border转换', () => {
+  const input = read('test/fixtures/border.css')
+  const output = read('test/fixtures/border.out.css')
+  expect(postcss(plugin({})).process(input).css).toEqual(output)
+})
+test('测试lineheight转换', () => {
+  const input = read('test/fixtures/lineheight.css')
+  const output = read('test/fixtures/lineheight.out.css')
+  expect(postcss(plugin({})).process(input).css).toEqual(output)
+})
+
+test('测试fontsize转换', () => {
+  const input = read('test/fixtures/fontsize.css')
+  const output = read('test/fixtures/fontsize.out.css')
+  expect(postcss(plugin({})).process(input).css).toEqual(output)
+})
+
+test('测试fontfamily转换', () => {
+  const input = read('test/fixtures/font-family.css')
+  const output = read('test/fixtures/font-family.out.css')
+  expect(postcss(plugin({})).process(input).css).toEqual(output)
+})
